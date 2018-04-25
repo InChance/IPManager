10d3340085c03c46c872704a2bc122ffc478535c
ni_menhao@sina.com <li0708li0708>
2018-04-25 11:28:42 +0800

    优化webpack配置

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: resolve('dist'),
         publicPath: '/',
         chunkFilename: '[name].[hash].js'
     },
